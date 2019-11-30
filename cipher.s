ea8ff21244937f021600bb3134150e1b52a22898
Ziad M. Sakr (zsakr) <zsakr@netlab03.cs.trincoll.edu>
2019-11-30 18:51:24 -0500

    Need to work on encrypt and decrypt

--- cipher.s
+++ cipher.s
@@ -10,12 +10,24 @@ cipher:
 			andl		$-16, %esp
 			subl		$32,	%esp
 
-			movl		12(%ebp), %eax				# Get word
-			movl		16(%ebp), %ebx				# Get option
-			movl		20(%ecx), %ecx				#	number of shift s
+			movl		12(%ebp), %esi				# Get word
+			movl		16(%ebp), %eax				# Get direction
+			movl		20(%ecx), %ecx				#	number of shifts
+
+			movl		$0x9E3779B9, %edx			# initalize the round constand amd keep in edx
+
+.loop:
 
 
+			cmpl		$1, %eax
+			je			encrypt
 
+.decrypt:
+			jmp 	end
+.encrypt:
+
+
+.end:
 
 			leave
 			ret
