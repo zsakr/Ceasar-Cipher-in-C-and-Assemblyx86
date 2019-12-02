dd03c0e04c4cd002b6e8695c63c31532aa89982e
Ziad M. Sakr (zsakr) <zsakr@netlab17.cs.trincoll.edu>
2019-12-01 21:30:43 -0500

    Need to work on encrypt and decrypt

--- cipher.c
+++ cipher.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define N 20
-extern int cipher( int n);
+extern char cipher(char c, int direcion, int shift);
+
 
 int main(){
-
-	int option;
+	char c;
+	int direcion;
 	int shift,i;
 	char str[N];
 	char exit[N];
+	
+	char fun[N];
 
-	printf("Enter a word: ");
-		scanf("%[^\n]*c", str);
+
 
 	while(1){
+		i = 0;
+		printf("Enter a word: ");
+		while((c = getchar()) != '\n'){
+			fun[i] = tolower(c);
+			i++;
+		}
 		printf("(1) Encrypt (2) Decrypt: \n");
-		scanf("%d", &option);
+		scanf("%d", &direcion);
 		printf("shift: ");
 		scanf("%d", &shift);
-		printf("Do another(Y/N)\n");
+		
+		for(i = 0; i < N; i++)
+			fun[i] = cipher(fun[i], direcion, shift);
+		for(i = 0; i < N; i++)	
+			printf("%c", fun[i]);
+			
+		printf("\nDo another(Y/N)\n");
 		scanf("%s", exit);
 
 			 if(strcmp(exit,"Y") == 0){
@@ -29,7 +44,9 @@ int main(){
 			}
 
 	}
-//printf("%d: ", cipher(shift));
 
 
 }
+
+
+
