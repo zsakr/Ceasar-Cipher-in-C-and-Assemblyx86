fafc1829d4d4f1608ba3e9c5c28ca8e7ab5f348a
Ziad Sakr <47642050+zsakr@users.noreply.github.com>
2019-12-07 14:13:43 -0500

    Final update

--- cipher.s
+++ cipher.s
@@ -3,30 +3,29 @@
 .global cipher
 .type cipher, %function
 
-
 cipher:
 
 			push		%ebp
 			movl 		%esp, %ebp
 			andl		$-16, %esp
-			subl		$32,	%esp
+			subl		$32,  %esp
 
-			movl		8(%ebp), %eax				# Get word
+			movl		8(%ebp),  %eax				# Get word
 			movl		12(%ebp), %edx				# Get direction
 			movl		16(%ebp), %ebx				# number of shifts
 
 
 .condition:
-			cmpl		$'a', %eax
+			cmpl		$'a', %eax  #a
 			jl			.end
-			cmpl		$'z', %eax
+			cmpl		$'z', %eax #z
 			jg			.end	
 			
 			cmpl		$1, %edx
 			je			.encrypt
 			cmpl		$2, %edx
-			
-			je			.encrypt
+			je			.decrypt
+
 
 .decrypt:
 
@@ -40,15 +39,13 @@ cipher:
 			addl		%ebx, %eax
 			cmpl		$'z', %eax
 			jg			.enc
-			
-			
 			jmp     	.end
 			
 			
 .dec:	
 			addl		$'z', %eax
 			subl		$'a', %eax
-			addl		$1,%eax
+			addl		$1,   %eax
 			
 			jmp 		.end
 			
@@ -61,6 +58,7 @@ cipher:
 
 
 .end:
-			movl %eax, 8(%ebp)
+			movl %eax, 12(%ebp)
+			
 			leave
 			ret
